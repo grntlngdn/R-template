a7a58c27099a56799702b8df173e5a4f4ea89819
grntlngdn <grntlngdn@gmail.com>
2019-01-07 18:11:39 -0600

    Changing template to be in subfolder so that the git files won't be in the same folder and won't need to be dealt with when copying the template. I then added the shell script to the repository. I had been keeping it in my home directory so that it can be easily run when I first open Git Bash

---
+++ directory-from-template.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Author : Grant Langdon
+# Copyright (c) https://github.com/grntlngdn
+
+
+TEMP=/c/users/grntl/OneDrive/Documents/r-projects/new-project-creator/template
+RPROJDIR=/c/users/grntl/OneDrive/Documents/r-projects/harvardx-125-data-science
+
+cd $TEMP
+FILES=`ls *.R`
+
+cd $RPROJDIR
+echo "Name R project:"
+read PROJECT
+if [ -e ./$PROJECT ]
+then
+   cd $PROJECT
+   PFILES=`ls *.R`
+   CFILES=($(comm -12 <(printf '%s\n' "${FILES[@]}") <(printf '%s\n' "${PFILES[@]}")))
+   cd ..
+else
+   mkdir $PROJECT
+   CFILES=()
+fi
+if [ ${#CFILES[@]} -eq 0 ]
+then
+   echo "What is the primary subject of this project?"
+   read SUBJECT
+   cp -r $TEMP .
+   mv -T template $PROJECT
+   cd $PROJECT
+   for FILE in $FILES
+   do
+      sed -i "s/\[Subject\]/$SUBJECT/g" $FILE
+   done
+else
+   echo this directory already contains R files that would be over-written, so the template wasn\'t used
+fi
