61fd97ec294446632d1ec84fa4b832572504f15b
grntlngdn <grntlngdn@gmail.com>
2019-01-11 16:56:30 -0600

    Added feature of opening project in RStudio. Fixed bug for projects in subfolders when running after the project folder already exists in that subfolder and added prompt instructions about using that path based capability.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # r-project-template-and-creator
 This includes the template I use for my R projects and the directory-from-template shell script that creates an R project from it. The script names the new project folder based on user input and substitutes the name of the subject of analysis that the user provides into the relevant places for the objects etc.
-Changing the two directory variables should allow the script to work as expected.
+Changing the two directory variables and the path to rstudio.exe (in the rstudio function) should allow the script to work as expected.
--- directory-from-template.sh
+++ directory-from-template.sh
@@ -7,34 +7,47 @@
 TEMP=/c/users/grntl/OneDrive/Documents/r-projects/new-project-creator/template
 RPROJDIR=/c/users/grntl/OneDrive/Documents/r-projects/harvardx-125-data-science
 
+#Function to open file with Rstudio
+rstudio () {
+   /c/'Program Files'/RStudio/bin/rstudio.exe $1 &#change this to represent where rstudio.exe is
+}
+
 cd $TEMP
 FILES=`ls *.R`
 
+#Note that when entering project name you can enter it as a subdirectory of an existing directory i.e. existing/project
 cd $RPROJDIR
-echo "Name R project:"
+echo "R project name (use relative path like 'subfolder/project' to create project in subfolder):"
 read PROJECT
+
+#If the project's directory already exists this gets a list of files in common between it and the template; if not it makes the directory
 if [ -e ./$PROJECT ]
 then
    cd $PROJECT
    PFILES=`ls *.R`
    CFILES=($(comm -12 <(printf '%s\n' "${FILES[@]}") <(printf '%s\n' "${PFILES[@]}")))
-   cd ..
+   cd `echo $PROJECT | sed -e 's/^[^\/]*/\.\./' -e 's|/[^\/]*|/\.\.|'`
 else
    mkdir $PROJECT
    CFILES=()
 fi
+#If no R files would be overwritten by doing so the template is copied and modified based on user input
 if [ ${#CFILES[@]} -eq 0 ]
 then
    echo "What is the primary subject of this project?"
    read SUBJECT
    cp -r $TEMP .
    mv -T template $PROJECT
+
    cd $PROJECT
-   mv template.Rproj $PROJECT.Rproj
+   mv template.Rproj $SUBJECT.Rproj
    for FILE in $FILES
    do
       sed -i "s/\[Subject\]/$SUBJECT/g" $FILE
    done
 else
    echo this directory already contains R files that would be over-written, so the template wasn\'t used
+   cd $PROJECT
 fi
+RPROJ=`ls *.Rproj`
+rstudio $RPROJ
